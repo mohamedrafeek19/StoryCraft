8142c86144ea29ee68d6c837f8642c699d3a8b4b
mohamedrafeek19 <122321860+mohamedrafeek19@users.noreply.github.com>
2024-01-26 17:54:04 +0530

    Update storycraft.py

--- storycraft.py
+++ storycraft.py
@@ -5,7 +5,7 @@ import random
 from prompts import generate_structured_sentence,format_twist_response
 
 # Streamlit page configuration
-st.set_page_config(page_title="StoryCraft - Interactive Storyteller",layout="wide",favicon=storycraft)
+st.set_page_config(page_title="StoryCraft - Interactive Storyteller",layout="wide")
 
 # API configuration
 API_KEY = st.secrets["API_KEY"]
