fdae2e333a92bd5c75f5d1cee3446688760910f2
mohamedrafeek19 <122321860+mohamedrafeek19@users.noreply.github.com>
2024-01-27 11:16:57 +0530

    Update storycraft.py

--- storycraft.py
+++ storycraft.py
@@ -91,7 +91,7 @@ style = """
     }
         /* Increase sidebar width */
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 600px;  /* Adjust the width as needed */
+        width: 1000px;  /* Adjust the width as needed */
         margin-left: -350px;  /* Adjust based on the width */
     }
 </style>
